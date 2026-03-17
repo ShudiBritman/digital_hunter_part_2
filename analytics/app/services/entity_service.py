b195c1c62348619034e026335ac000d3420fe61c
Shudi <sdbritman@gmail.comgit config --listgit config --listgit config --global user.name>
2026-03-17 15:08:55 +0200

    change to class and add process func

--- analytics/app/services/entity_service.py
+++ analytics/app/services/entity_service.py
@@ -1,16 +1,24 @@
 import matplotlib as plt
+from dal.sql_dal import get_target_location_by_day
 
 
 
-def plot_entity(locations):
-    x = [row[0] for row in locations]
-    y = [row[1] for row in locations]
+class Processor:
+    def Process(entity_id):
+        locations = get_target_location_by_day()
+        Processor.plot_entity(locations)
 
-    plt.figure()
-    plt.plot(x, y, marker="o")
-    plt.scatter(x[0], y[0], color="green", label="start")
-    plt.scatter(x[0], y[0], color="red", label="end")
 
-    plt.show()
+    @staticmethod
+    def plot_entity(locations):
+        x = [row[0] for row in locations]
+        y = [row[1] for row in locations]
+
+        plt.figure()
+        plt.plot(x, y, marker="o")
+        plt.scatter(x[0], y[0], color="green", label="start")
+        plt.scatter(x[0], y[0], color="red", label="end")
+
+        plt.show()
 
 
