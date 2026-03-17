10a349f73502cf1237acabc53d2bc1cc960e51ad
Shudi <sdbritman@gmail.comgit config --listgit config --listgit config --global user.name>
2026-03-17 15:04:22 +0200

    add plot entity func

--- analytics/app/services/entity_service.py
+++ analytics/app/services/entity_service.py
@@ -1,3 +1,16 @@
+import matplotlib as plt
 
+
+
+def plot_entity(locations):
+    x = [row[0] for row in locations]
+    y = [row[1] for row in locations]
+
+    plt.figure()
+    plt.plot(x, y, marker="o")
+    plt.scatter(x[0], y[0], color="green", label="start")
+    plt.scatter(x[0], y[0], color="red", label="end")
+
+    plt.show()
 
 
