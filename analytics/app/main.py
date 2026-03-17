525c630fd7820781b6d6613e9c3a402febbfe513
Shudi <sdbritman@gmail.comgit config --listgit config --listgit config --global user.name>
2026-03-17 10:42:18 +0200

    add main of server

---
+++ analytics/app/main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+from routes.routes import router
+import uvicorn
+
+
+app = FastAPI()
+
+app.include_router(
+    router=router
+)
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=8000)
