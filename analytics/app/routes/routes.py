ca86e402c94261244be76831837bf68b85344f4a
Shudi <sdbritman@gmail.comgit config --listgit config --listgit config --global user.name>
2026-03-17 12:10:37 +0200

    add route

--- analytics/app/routes/routes.py
+++ analytics/app/routes/routes.py
@@ -12,6 +12,7 @@ def get_high_priority_moved_targets():
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
+@router.get("/count_signal_type")
 def get_count_signal_type():
     try:
         return count_signal_type()
