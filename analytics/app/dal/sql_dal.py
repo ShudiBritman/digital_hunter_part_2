5f4777e53a17ad2102672cfff640b8d534209d97
Shudi <sdbritman@gmail.comgit config --listgit config --listgit config --global user.name>
2026-03-17 11:45:13 +0200

    add logger

--- analytics/app/dal/sql_dal.py
+++ analytics/app/dal/sql_dal.py
@@ -1,5 +1,12 @@
 from app.db_connection.sql_connection import DataBase
+import logging
 
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] %(message)s",
+)
+logger = logging.getLogger(__name__)
 
 conn = DataBase.get_connection()
 
@@ -12,9 +19,9 @@ def high_priority_moved_targets():
                 WHERE priority IN (1, 2)
                 AND distance_moved > 5'''
     cursor = conn.cursor()
-    cursor.execute()
+    cursor.execute(query)
     value = cursor.fetchall()
-
+    logger.info("result", value)
     cursor.close()
 
     return value
