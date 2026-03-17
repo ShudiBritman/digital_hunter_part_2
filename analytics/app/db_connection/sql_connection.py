144cc6900a1db1b816f77ea6ddafcf0414e4436f
Shudi <sdbritman@gmail.comgit config --listgit config --listgit config --global user.name>
2026-03-17 11:22:01 +0200

    fix self

--- analytics/app/db_connection/sql_connection.py
+++ analytics/app/db_connection/sql_connection.py
@@ -2,7 +2,7 @@ import mysql.connector
 import os
 
 host = os.getenv("MYSQL_HOST")
-port = int(os.getenv("MYSQL_PORT"))
+port = os.getenv("MYSQL_PORT")
 user = os.getenv("MYSQL_USER")
 password = os.getenv("MYSQL_PASSWORD")
 database = os.getenv("MYSQL_DB")
@@ -11,11 +11,11 @@ database = os.getenv("MYSQL_DB")
 class DataBase:
     
     @staticmethod
-    def get_connection(self):
+    def get_connection():
         return mysql.connector.connect(
-            host=self.host,
-            port=self.port,
-            user=self.user,
-            password=self.password,
-            database=self.database
+            host=host,
+            port=port,
+            user=user,
+            password=password,
+            database=database
         )
