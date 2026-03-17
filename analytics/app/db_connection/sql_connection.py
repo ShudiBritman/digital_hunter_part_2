dd8b98207a813cd8054878007f53b149ccc08d1e
Shudi <sdbritman@gmail.comgit config --listgit config --listgit config --global user.name>
2026-03-17 11:36:01 +0200

    fix env

--- analytics/app/db_connection/sql_connection.py
+++ analytics/app/db_connection/sql_connection.py
@@ -1,8 +1,16 @@
 import mysql.connector
 import os
+import logging
+
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] %(message)s",
+)
+logger = logging.getLogger(__name__)
 
 host = os.getenv("MYSQL_HOST")
-port = os.getenv("MYSQL_PORT")
+port = int(os.getenv("MYSQL_PORT", 3306))
 user = os.getenv("MYSQL_USER")
 password = os.getenv("MYSQL_PASSWORD")
 database = os.getenv("MYSQL_DB")
@@ -12,10 +20,13 @@ class DataBase:
     
     @staticmethod
     def get_connection():
-        return mysql.connector.connect(
-            host=host,
-            port=port,
-            user=user,
-            password=password,
-            database=database
-        )
+        try:
+            return mysql.connector.connect(
+                host=host,
+                port=port,
+                user=user,
+                password=password,
+                database=database
+            )
+        except mysql.connector.Error as err:
+            logger.error("Error: %s", err)
