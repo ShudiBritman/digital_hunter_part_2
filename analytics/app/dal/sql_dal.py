e052a346ab701505d0da220accf95d0b3cde7620
Shudi <sdbritman@gmail.comgit config --listgit config --listgit config --global user.name>
2026-03-17 11:14:05 +0200

    add first query

--- analytics/app/dal/sql_dal.py
+++ analytics/app/dal/sql_dal.py
@@ -4,7 +4,12 @@ from app.db_connection.sql_connection import DataBase
 conn = DataBase.get_connection()
 
 def high_priority_moved_targets():
-    query = ""
+    query = '''SELECT 
+                entity_id,
+                target_name,
+                priority_level
+                FROM targets
+                WHERE priority IN (1, 2)'''
     cursor = conn.cursor()
     cursor.execute()
     value = cursor.fetchall()
